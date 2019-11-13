135ee690e53f64fe66acfb88156529c4793540b2
дом <@192.168.56.1>
2019-11-13 14:47:06 +0300

    Небольшое изменение в ModelProvider.

--- H2DataBaseApplication/src/ModelProvider.java
+++ H2DataBaseApplication/src/ModelProvider.java
@@ -1,18 +1,14 @@
 import java.util.List;
 
 public class ModelProvider {
-	
-	private Storage storage;
+
 	private List<User> usersData;
 	
 	public ModelProvider(Storage storage) throws Exception {
-		this.storage = storage;
 		usersData = storage.getUsersDataSet(false, false);
 	}
 	
 	public List<User> getUsersData(){
 		return usersData;
 	}
-	
-	
 }
