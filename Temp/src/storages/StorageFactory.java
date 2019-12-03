abda8c2ab97864d93f125ed05eddc0d9a1cb6611
дом <@169.254.170.118>
2019-12-03 03:13:41 +0300

    Добавлена схема для хранилища данных. Теперь в классе StorageFactory используется механизм доступа к соответствующей точке расширения и расширению для базы данных.

--- Temp/src/storages/StorageFactory.java
+++ Temp/src/storages/StorageFactory.java
@@ -1,18 +1,29 @@
 package storages;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.IConfigurationElement;
 /**
  * Фабрика для создания объектов классов, реализующих интерфейс Storage.
  */
 public class StorageFactory {
+	
+	private final String storageExtensionID = "Temp.stores";
 	
 	/**
 	 * Метод возвращает хранилище данных пользователей.
 	 * @param storageName - имя хранилища
 	 */
 	public Storage getStorage(String storageName) {
+		IExtensionRegistry reg = Platform.getExtensionRegistry();
+		IConfigurationElement[] extensions = reg.getConfigurationElementsFor(storageExtensionID);
 		Storage storage;
-		if (storageName.startsWith("jdbc:h2:")) storage = new DataBase(storageName); else 
-			if (storageName.endsWith(".txt")) storage = new FileStorage(storageName); else
-				storage = new DataBase("jdbc:h2:~/test");
+		for (IConfigurationElement extension : extensions) 
+			if (storageName.equals(extension.getAttribute("name"))) {
+				storage = new DataBase(storageName);
+				return storage;
+			}
+		if (storageName.endsWith(".txt")) storage = new FileStorage(storageName); else
+			storage = new DataBase("jdbc:h2:~/test");
 		return storage;
 	}
 }
